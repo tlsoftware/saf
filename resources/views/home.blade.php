50b20e61b7c03dcf8fbf60e071db63a9268c46c6
David Fuentes <ing.davidfuentes@outlook.com>
2017-06-13 22:37:30 -0400

    Add Tab to Home Only View

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,40 +4,66 @@
 <div class="container-fluid">
     <div class="row">
         <div class="col-md-12 col-md-offset-0">
-            <div class="panel panel-primary">
-                <div class="panel-heading">
-                        <div class="row">
-                            <div id="left">
-                                <span id="left-heading"><strong>Clientes Pendientes por Gestionar </strong> </span>
-                            </div>
-                            <div class="pull-right">
-                                <span class="label label-danger" id="right-heading">Total Pendientes: {{ $customers->count() }}</span>
+            <div id="exTab3" class="container-fluid">
+                <ul  class="nav nav-pills">
+                    <li class="active">
+                        <a  href="#home" data-toggle="tab">Home</a>
+                    </li>
+                    <li>
+                        <a href="#potenciales" data-toggle="tab">Potenciales</a>
+                    </li>
+                    <li>
+                        <a href="#muestras" data-toggle="tab">Muestras</a>
+                    </li>
+                    <li>
+                        <a href="#activos" data-toggle="tab">Activos</a>
+                    </li>
+                    <li>
+                        <a href="#rechazos" data-toggle="tab">Rechazos</a>
+                    </li>
+                    <li>
+                        <a href="#bajas" data-toggle="tab">Bajas</a>
+                    </li>
+                    <li>
+                        <a href="#todos" data-toggle="tab">Todos</a>
+                    </li>
+                </ul>
+                <div class="tab-content clearfix">
+                    <div class="tab-pane active" id="home">
+                        <div class="panel panel-primary">
+                            <div class="panel-heading">
+                                <div class="row">
+                                <div id="left">
+                                    <span id="left-heading"><strong>Clientes Pendientes por Gestionar </strong> </span>
+                                </div>
+                                <div class="pull-right">
+                                    <span class="label label-danger" id="right-heading">Total Pendientes: {{ $customers->count() }}</span>
+                                </div>
                             </div>
                         </div>
-                </div>
-                    <div class="panel-body">
-                        <a href="{{ route('customers.create') }}" class="btn btn-info" id="new_customer">Nuevo Cliente</a>
-                        <div class="pull-right">
-                            <span class="glyphicon glyphicon-tag" style="font-size: larger" aria-hidden="true"> <strong> {{ $status }} </strong> </span>
+                        <div class="panel-body">
+                            <a href="{{ route('customers.create') }}" class="btn btn-info" id="new_customer">Nuevo Cliente</a>
                         </div>
-                    </div>
                         <hr>
+
                         <table class="table table-striped table-bordered table-hover table-condensed dataTable" id="home_table">
                             <thead>
-                            <th>Gestión</th>
-                            <th>Nombre Comercial</th>
-                            <th>Persona de Contacto</th>
-                            <th>Teléfono</th>
-                            <!-- Solo mostrar en case de que sea Administrador -->
-                            @if(Auth::user()->admin)
-                                <th>Responsable</th>
-                            @endif
-                            <th>Última Gestión</th>
-                            <th>Próxima Gestión</th>
-                            @if($status == 'Todos los Clientes')
-                                <th>Estatus General</th>
-                            @endif
-                            <th>Estatus Detallado</th>
+                            <tr>
+                                <th>Gestión</th>
+                                <th>Nombre Comercial</th>
+                                <th>Persona de Contacto</th>
+                                <th>Teléfono</th>
+                                <!-- Solo mostrar en case de que sea Administrador -->
+                                @if(Auth::user()->admin)
+                                    <th>Responsable</th>
+                                @endif
+                                <th>Última Gestión</th>
+                                <th>Próxima Gestión</th>
+                                @if($status == 'Todos los Clientes')
+                                    <th>Estatus General</th>
+                                @endif
+                                <th>Estatus Detallado</th>
+                            </tr>
                             </thead>
                             <tbody>
                             @foreach($customers as $customer)
@@ -52,14 +78,13 @@
                                     @if(Auth::user()->admin)
                                         <td>{{ $customer->user()->first()->name }}</td>
                                     @endif
-
-                                    <!-- Validar si el Cliente Posee Gestiones -->
+                                <!-- Validar si el Cliente Posee Gestiones -->
                                     @if($customer->managements->count())
                                         <td>{{ \Carbon\Carbon::parse($customer->managements->last()->created_at)->format('d-m-Y') }}</td>
                                     @else
                                         <td> N/A </td>
                                     @endif
-                                    <!-- PROXIMA GESTION -->
+                                <!-- PROXIMA GESTION -->
                                     <td>{{ Carbon\Carbon::parse($customer->next_mng)->format('d-m-Y') }}</td>
                                     <!-- STATUS -->
                                     @if($status == 'Todos los Clientes')
@@ -70,7 +95,9 @@
                             @endforeach
                             </tbody>
                         </table>
+                        </div>
                     </div>
+                </div>
             </div>
         </div>
     </div>
