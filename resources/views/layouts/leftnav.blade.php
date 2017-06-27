67235317bf7b775aa7b67e8d395023376628b24d
David Fuentes <ing.davidfuentes@outlook.com>
2017-06-26 21:06:50 -0400

    Change Home Name

--- resources/views/layouts/leftnav.blade.php
+++ resources/views/layouts/leftnav.blade.php
@@ -1,4 +1,4 @@
-<li ><a href="{{ action('HomeController@index') }}">Home<span class="sr-only"></span></a></li>
+<li ><a href="{{ action('HomeController@index') }}">Pendientes<span class="sr-only"></span></a></li>
 @if(Auth::user()->isAdmin())
     <li class="dropdown">
         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
