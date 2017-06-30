61bb11c3a8b5426790cc892f4a790097669afd5d
David Fuentes <ing.davidfuentes@outlook.com>
2017-06-29 21:30:36 -0400

    Fix Windows Size on Management View

--- resources/views/managements/show.blade.php
+++ resources/views/managements/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
+    <div class="container-fluid">
         <div class="row">
             <div class="col-md-12 col-md-offset-0">
                 <div class="panel panel-primary">
