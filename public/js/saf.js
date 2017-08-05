45125b920ba71d12b479d3ca422fb9dcdb2f9666
David Fuentes <ing.davidfuentes@outlook.com>
2017-08-05 14:12:30 -0400

    Change Load Datatables 2

--- public/js/saf.js
+++ public/js/saf.js
@@ -32,10 +32,6 @@ $(document).ready(function() {
         });
     });
 
-    $('#home_table').DataTable();
-    $('#migrate_table').DataTable();
-
-    $.fn.dataTable.moment( 'DD-MM-YYYY' );
 
     $.extend( true, $.fn.dataTable.defaults, {
         "oLanguage": {
@@ -63,6 +59,11 @@ $(document).ready(function() {
             }
         }
     });
+
+    $.fn.dataTable.moment( 'D-M-Y' );
+
+    $('#home_table').DataTable();
+    $('#migrate_table').DataTable();
 
     $('#btn-addVenta').click(function (e) {
         var rut = $('#rut').val();
